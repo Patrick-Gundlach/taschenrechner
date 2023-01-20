c8898ef6b3e3c76131c53c85f9e66a60e889d5b7
Patrick Gundlach <123129965+Patrick-Gundlach@users.noreply.github.com>
2023-01-20 01:27:42 +0100

    PI files Uploads

---
+++ function.js
@@ -0,0 +1,24 @@
+function appendOperation(operation){
+    document.getElementById('resultArea').innerHTML += operation;
+}
+function caculateResult(){
+    let container = document.getElementById('resultArea');
+    let result = eval(container.innerHTML);
+
+    container.innerHTML = result;
+
+}
+function deleteLast(){
+    let container = document.getElementById('resultArea');
+    if (container.innerHTML.endsWith(' ')){
+    container.innerHTML = container.innerHTML.slice(0, -3)
+    } else {
+    container.innerHTML = container.innerHTML.slice(0, -1)
+}
+}
+
+function deleteAll(){
+    let container = document.getElementById('resultArea');
+
+    container.innerHTML = container.innerHTML.slice(0, -500)
+}
